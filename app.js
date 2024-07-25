160fed404ed2544c55bc054d83d9454839a41077
Krishna B <166964261+W-i-z-z-k-i-d@users.noreply.github.com>
2024-07-26 01:21:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function wishMe(){
     var hour = day.getHours();
 
     if(hour>=0 && hour<12){
-        speak("Good Morning Shivam...")
+        speak("Good Morning Krishna...")
     }
 
     else if(hour>12 && hour<17){
